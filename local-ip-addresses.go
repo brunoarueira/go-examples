eda463d79155625be19dd12d00ac365893e20c40
Bruno Arueira <contato@brunoarueira.com>
2015-01-24 10:43:06 -0200

    Get local IP addresses
    http://golang-examples.tumblr.com/post/99458329439/get-local-ip-addresses

---
+++ local-ip-addresses.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "net"
+  "fmt"
+)
+
+func main() {
+  list, err := net.Interfaces()
+
+  if err != nil {
+    panic(err)
+  }
+
+  for i, iface := range list {
+    fmt.Printf("%d name=%s %v\n", i, iface.Name, iface)
+    addrs, err := iface.Addrs()
+
+    if err != nil {
+      panic(err)
+    }
+
+    for j, addr := range addrs {
+      fmt.Printf(" %d %v\n", j, addr)
+    }
+  }
+}
